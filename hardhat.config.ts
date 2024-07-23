e3a1ae1e1ea1aeabd18185fc41769e9a0447df8c
Miftahul Arifin <miftahul97@gmail.com>
2024-07-23 20:22:55 +0700

    init

---
+++ hardhat.config.ts
@@ -0,0 +1,17 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from 'dotenv'; 
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/", //URL of the RPC node for Swisstronik.
+      accounts: [process.env.ACCOUNT_KEY as string], //Your private key starting with "0x" 
+      //Make sure you have enough funds in this wallet to deploy the smart contract
+    },
+  },
+};
+
+export default config;
